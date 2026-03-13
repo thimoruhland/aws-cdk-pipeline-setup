a1d80051bc1eb89cf885c9f8ac74c48add46fa94
Thimo Ruhland <thimoru@MacBook-Air-von-Thimo.local>
2026-03-13 19:38:47 +0100

    add pipeline

--- src/stacks/pipeline_stack.py
+++ src/stacks/pipeline_stack.py
@@ -1,36 +1,29 @@
 from aws_cdk import Stack
-from aws_cdk import (
-    Stack
-)
 from constructs import Construct
 from aws_cdk import pipelines
 from src.stages.application_stage import ApplicationStage
 
 
 class PipelineStack(Stack):
-
     def __init__(self, scope: Construct, construct_id: str, **kwargs):
         super().__init__(scope, construct_id, **kwargs)
 
         pipeline = pipelines.CodePipeline(
             self,
-            "Pipeline",
-            pipeline_name="github-pipeline-dev",
+            "CdkPipeline",
             synth=pipelines.ShellStep(
                 "Synth",
                 input=pipelines.CodePipelineSource.connection(
-                    "DEIN_GITHUB_USERNAME/aws-cdk-pipeline-setup",
+                    "thimoruhland/aws-cdk-pipeline-setup",
                     "main",
-                    connection_arn="arn:aws:codeconnections:eu-central-1:735910967196:connection/47dc864b-e5a7-4f37-ac09-54d70e37c623"
+                    connection_arn="arn:aws:codeconnections:eu-central-1:735910967196:connection/47dc864b-e5a7-4f37-ac09-54d70e37c623",
                 ),
                 commands=[
-                    "pip install -r requirements.txt",
+                    "python -m pip install -r requirements.txt",
                     "npm install -g aws-cdk",
-                    "cdk synth"
+                    "cdk synth",
                 ],
             ),
         )
 
-        pipeline.add_stage(
-            ApplicationStage(self, "Prod")
-        )
+        pipeline.add_stage(ApplicationStage(self, "Prod"))
