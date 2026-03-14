71fb8d46f53482ad7dea22a59affc79c12a583e3
Thimo Ruhland <thimoru@MacBook-Air-von-Thimo.local>
2026-03-14 10:05:19 +0100

    resolve dependencies

--- tests/unit/test_aws_cdk_pipeline_setup_stack.py
+++ tests/unit/test_aws_cdk_pipeline_setup_stack.py
@@ -1,7 +1,7 @@
 import aws_cdk as core
 import aws_cdk.assertions as assertions
 
-from aws_cdk_pipeline_setup.aws_cdk_pipeline_setup_stack import AwsCdkPipelineSetupStack
+from aws_cdk_pipeline_setup.aws_cdk_pipeline_setup_stack import AwsCdkPipelineSetupStack # type: ignore
 
 # example tests. To run these tests, uncomment this file along with the example
 # resource in aws_cdk_pipeline_setup/aws_cdk_pipeline_setup_stack.py
